088bae8d1c7b2ee354c87a1a7818c82fc593f400
Parwez Alam <your-email@gmail.com>
2026-06-18 00:53:15 +0530

    Add crypto news page

--- PHASE 2/page3 project/CRIPTO-DASHBOARD/js/news.js
+++ PHASE 2/page3 project/CRIPTO-DASHBOARD/js/news.js
@@ -1,1 +1,35 @@
-console.log("News Page Loaded");
+const newsContainer =
+document.getElementById("newsContainer");
+
+const demoNews = [
+
+    {
+        title: "Bitcoin Surges Past Key Resistance",
+        source: "Crypto News"
+    },
+
+    {
+        title: "Ethereum ETF Sees Increased Demand",
+        source: "Market Watch"
+    },
+
+    {
+        title: "Solana Network Activity Hits New High",
+        source: "Blockchain Daily"
+    }
+
+];
+
+demoNews.forEach(news => {
+
+    newsContainer.innerHTML += `
+        <div class="card">
+
+            <h3>${news.title}</h3>
+
+            <p>${news.source}</p>
+
+        </div>
+    `;
+
+});
