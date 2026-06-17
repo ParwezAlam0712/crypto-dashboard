9adfe4ea818e846a04ea7ad5343a1be67f7fdfae
Parwez Alam <your-email@gmail.com>
2026-06-17 20:22:53 +0530

    feat: add dark mode support

---
+++ PHASE 2/page3 project/CRIPTO-DASHBOARD/js/theme.js
@@ -0,0 +1,21 @@
+const themeBtn = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+themeBtn?.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        localStorage.setItem("theme", "dark");
+
+    } else {
+
+        localStorage.setItem("theme", "light");
+
+    }
+
+});
